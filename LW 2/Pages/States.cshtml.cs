07617c6764132b29a43d1d9be278ddb089afb554
Fallmore <slava_samodurov@mail.ru>
2025-03-17 17:25:45 +0400

    Адаптировано под Razor

---
+++ LW 2/Pages/States.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LW_2.Pages
+{
+    public class StatesModel : PageModel
+    {
+        private readonly ILogger<StatesModel> _logger;
+
+        public StatesModel(ILogger<StatesModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
